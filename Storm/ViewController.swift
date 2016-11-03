db0caebdcd1b46b008b2c6742fe68be7616fb7c5
小飼きわみ <kiwamikogai@me.com>
2016-11-03 19:49:35 +0900

    Initial Commit

---
+++ Storm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Storm
+//
+//  Created by Kiwami on 2016/11/03.
+//  Copyright © 2016年 Kiwami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
